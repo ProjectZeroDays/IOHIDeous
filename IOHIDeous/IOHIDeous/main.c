1447c43d68ddabca0bdb65a7d5f7f129d44fbca8
shinvou <shinvou@gmail.com>
2018-01-07 13:38:46 +0100

    Xcode-ify

---
+++ IOHIDeous/IOHIDeous/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  IOHIDeous
+//
+//  Created by Timm Kandziora on 06.01.18.
+//  Copyright © 2018 Timm Kandziora. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
